2cf63384e04849d59d1f88d93ff6e9a4927b18f0
Eric Laufer <eric@Erics-MacBook-Pro.local>
2023-12-30 21:41:51 -0500

    wip

--- nvim/install_mac.sh
+++ nvim/install_mac.sh
@@ -1,16 +1,20 @@
 # #!/bin/bash
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
 tar xzf nvim-macos.tar.gz
+rm nvim-macos.tar.gz
 
 # Check if "/usr/local/bin" is in $PATH
 if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; then
     echo "Warning: /usr/local/bin is not in your \$PATH. Consider adding it for better compatibility."
 else
     echo "/usr/local/bin is in your \$PATH."
-    rm -rf /usr/local/bin/nvim-macos/
-    rm /usr/local/bin/nvim
-    mv nvim-macos/ /usr/local/bin
+    sudo rm -rf /usr/local/bin/nvim-macos/
+    sudo rm /usr/local/bin/nvim
+    sudo mv nvim-macos/ /usr/local/bin
+    sudo ln -s /usr/local/bin/nvim-macos/bin/nvim /usr/local/bin/n
     sudo ln -s /usr/local/bin/nvim-macos/bin/nvim /usr/local/bin/nvim
-
+    mkdir ~/.config/nvim
+    touch ~/.config/nvim/init.lua
+    mkdir ~/.config/nvim/lua
 fi
 
