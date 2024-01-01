d03ef4c4ca2a3a0526714fc13d0111c10139a459
Eric Laufer <eric@dreamfold.ai>
2024-01-01 10:32:10 -0800

    install scripts

--- nvim/install.sh
+++ nvim/install.sh
@@ -7,7 +7,14 @@ if [[ $(uname) == "Darwin" ]]; then
 # Check if the system is Linux
 elif [[ $(uname) == "Linux" ]]; then
     echo "System is Linux."
-# If neither macOS nor Linux
+    sudo apt update && sudo apt upgrade
+    sudo apt install curl
+    sudo apt-get install fuse libfuse2
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
+    sudo mv nvim.appimage /usr/local/bin/nvim
+    mkdir -p ~/.config/nvim
+    cp -r nvim/* ~/.config/nvim/
 else
     echo "Unsupported operating system."
 fi
--- nvim/install_linux.sh
+++
@@ -1,5 +0,0 @@
-# Prerequisites
-sudo apt update && sudo apt upgrade
-sudo apt install curl
-sudo apt-get install fuse libfuse2
-sudo mv nvim.appimage /usr/local/bin/nvim
